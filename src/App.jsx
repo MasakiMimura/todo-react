3403d5438998baf6b7f139db98f380832c845112
Masaki Mimura <62504066+MasakiMimura@users.noreply.github.com>
2021-06-12 11:25:43 +0900

    Reactでの実装を意識したモックに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいい"
+  ]);
+  const [completeTodos, setCompleteTodos] = useState(["うううう"]);
   return (
     <>
-      <div class="input-area">
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div class="incomplete-area">
-        <p class="title">未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <li class="list-row">
-            <div class="div-row">
-              <p>ああああ</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
-          <li class="list-row">
-            <div class="div-row">
-              <p>いいいい</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
+          {incompleteTodos.map((todo) => {
+            return (
+              <li key={todo} className="list-row">
+                <div className="div-row">
+                  <p>{todo}</p>
+                  <button>完了</button>
+                  <button>削除</button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
-      <div class="complete-area">
-        <p class="title">完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
-          <li class="list-row">
-            <div class="div-row">
-              <p>うううう</p>
-              <button>戻す</button>
-            </div>
-          </li>
+          {completeTodos.map((todo) => {
+            return (
+              <li key={todo} className="list-row">
+                <div className="div-row">
+                  <p>{todo}</p>
+                  <button>完了</button>
+                  <button>削除</button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
