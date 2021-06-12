1c9583f356ef498e852673acaa431c0b1a90aab1
Masaki Mimura <62504066+MasakiMimura@users.noreply.github.com>
2021-06-12 10:51:32 +0900

    CSSでスタイリング

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,38 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div class="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div class="incomplete-area">
+        <p class="title">未完了のTODO</p>
         <ul>
-          <div>
-            <li>ああああ</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div>
-            <li>いいいい</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          <li class="list-row">
+            <div class="div-row">
+              <p>ああああ</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li class="list-row">
+            <div class="div-row">
+              <p>いいいい</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div class="complete-area">
+        <p class="title">完了のTODO</p>
         <ul>
-          <div>
-            <li>うううう</li>
-            <button>戻す</button>
-          </div>
+          <li class="list-row">
+            <div class="div-row">
+              <p>うううう</p>
+              <button>戻す</button>
+            </div>
+          </li>
         </ul>
       </div>
     </>
